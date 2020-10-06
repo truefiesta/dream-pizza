a8025f926efbd87fd4bb916e5cc832853d2c32e7
Yulia Karpova <karpova.jul@gmail.com>
2020-10-06 19:15:09 +1100

    Add mocks for jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
     verbose: true,
     rootDir: './src',
     moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+      '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+    },
 }
