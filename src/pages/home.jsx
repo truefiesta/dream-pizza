4e8c32c5548e44916aa2e40a2cef1048cebfc6b6
Yulia Karpova <karpova.jul@gmail.com>
2020-10-10 00:25:46 +1100

    Add to global styles

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../components/header/header.jsx";
+import Footer from "../components/footer/footer.jsx";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <main></main>
+      <Footer />
     </>
   );
 };
