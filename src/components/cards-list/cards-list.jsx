728a623bdfda595f11cf0454e34e022e881228ea
Yulia Karpova <karpova.jul@gmail.com>
2020-10-10 15:01:30 +1100

    Create CardList component

---
+++ src/components/cards-list/cards-list.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../card/card.jsx";
+import "./cards-list.css";
+
+const CardsList = () => {
+  return (
+    <ul className="cards-list">
+      <Card />
+    </ul>
+  );
+};
+
+export default CardsList;
