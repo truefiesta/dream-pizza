f720c28d0d3ac42b6ee7a8c14c1849d3f1ab8dd6
Yulia Karpova <karpova.jul@gmail.com>
2020-10-10 15:02:07 +1100

    Add new components to the Home page component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Header from "../components/header/header.jsx";
 import Footer from "../components/footer/footer.jsx";
 import Sort from "../components/sort/sort.jsx";
+import CardsList from "../components/cards-list/cards-list.jsx";
 
 const Home = () => {
   return (
     <>
       <Header />
       <main>
-        <Sort />
+        <div className="wrapper">
+          <h1 className="visually-hidden">Welcome to Dream Pizza</h1>
+          {/* TABS ?  */}
+          <Sort />
+          <h2 className="section-title">What’s new on our Menu</h2>
+          <CardsList />
+          {/* TABS ?  */}
+        </div>
       </main>
       <Footer />
     </>
