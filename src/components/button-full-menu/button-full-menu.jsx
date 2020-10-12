bd22f0de8d21b05fd25f15b8c143abe60c6fe557
Yulia Karpova <karpova.jul@gmail.com>
2020-10-12 15:52:52 +1100

    Add ButtonFullMenu component

---
+++ src/components/button-full-menu/button-full-menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./button-full-menu.css";
+
+const ButtonFullMenu = () => {
+  return (
+    <a
+      className="button button-full-menu home-page-button-full-menu"
+      href="/menu"
+    >
+      See full menu
+    </a>
+  );
+};
+
+export default ButtonFullMenu;
