8ba313294e073a11b9bda2bb3076b9f6dc9aaa8d
Yulia Karpova <karpova.jul@gmail.com>
2020-10-14 16:38:04 +1100

    Add map styles for differenent screens

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -4,17 +4,30 @@ import "./map.css";
 const Map = () => {
   return (
     <section className="map">
-      <div className="map-derails-container">
+      <div className="map-title-container">
         <h2 className="section-title">Our restaurants</h2>
       </div>
-
-      <div className="map-container">
-        <article className="map-pin-info">
-          <h3 className="map-pin-title">Dream Pizza - Rose Bay</h3>
-          <p className="map-pin-text">
-            516 Old South Head Rd, Rose Bay NSW 2029
-          </p>
-        </article>
+      <div className="map-content-wrapper">
+        <div className="map-container">
+          <article className="map-pin-info">
+            <h3 className="map-pin-title">
+              Dream Pizza - <span>Rose Bay</span>
+            </h3>
+            <p className="map-pin-text">
+              516 Old South Head Rd, Rose Bay NSW 2029
+            </p>
+          </article>
+        </div>
+        <div className="map-details-container">
+          <section className="map-pin-info">
+            <h3 className="map-pin-title">
+              Dream Pizza - <span>Rose Bay</span>
+            </h3>
+            <p className="map-pin-text">
+              516 Old South Head Rd, Rose Bay NSW 2029
+            </p>
+          </section>
+        </div>
       </div>
     </section>
   );
