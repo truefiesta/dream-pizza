ffa87cda17894fe935dd791fe75550a6606a8684
Yulia Karpova <karpova.jul@gmail.com>
2020-10-18 01:42:48 +1100

    Add pizza page

--- src/pages/pizza.jsx
+++ src/pages/pizza.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../components/header/header.jsx";
+import CardDetails from "../components/card-details/card-details.jsx";
+import Footer from "../components/footer/footer.jsx";
 
 const Pizza = () => {
-  return <div>Pizza</div>;
+  return (
+    <>
+      <Header />
+      <main className="pizza-page">
+        <div className="wrapper">
+          <h1 className="visually-hidden">Pizza page</h1>
+          <CardDetails />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Pizza;
