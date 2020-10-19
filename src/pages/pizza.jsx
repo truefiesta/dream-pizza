829316bc93dc4d605b679179ef81d8ab67df5dd1
Yulia Karpova <karpova.jul@gmail.com>
2020-10-19 23:45:33 +1100

    Add reviews to the pizza page

--- src/pages/pizza.jsx
+++ src/pages/pizza.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "../components/header/header.jsx";
 import CardDetails from "../components/card-details/card-details.jsx";
+import Review from "../components/review/review.jsx";
+import ReviewForm from "../components/review-form/review-form.jsx";
+import PrevNextControls from "../components/prev-next-controls/prev-next-controls.jsx";
 import Footer from "../components/footer/footer.jsx";
 
 const Pizza = () => {
@@ -11,6 +14,17 @@ const Pizza = () => {
         <div className="wrapper">
           <h1 className="visually-hidden">Pizza page</h1>
           <CardDetails />
+          <section className="reviews">
+            <div className="reviews-top">
+              <h2 className="section-title">Reviews</h2>
+              <p className="reviews-count">4.8 / 5 (365 reviews)</p>
+            </div>
+            <div className="review-container">
+              <Review />
+            </div>
+            <PrevNextControls />
+          </section>
+          <ReviewForm />
         </div>
       </main>
       <Footer />
