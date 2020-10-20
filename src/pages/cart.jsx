cea9062d09080b4285603fa12e513c1329c9368e
Yulia Karpova <karpova.jul@gmail.com>
2020-10-21 10:46:41 +1100

    Complete cart page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Header from "../components/header/header.jsx";
+import Breadcrumbs from "../components/breadcrumbs/breadcrumbs.jsx";
+import CardCart from "../components/card-cart/card-cart.jsx";
+import CartTotal from "../components/cart-total/cart-total.jsx";
+import Footer from "../components/footer/footer.jsx";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <>
+      <Header />
+      <main className="cart-page">
+        <div className="wrapper">
+          <Breadcrumbs />
+          <h1 className="main-title">My order</h1>
+          <div className="cart-container">
+            <ul className="cart-list">
+              <CardCart />
+              <CardCart />
+            </ul>
+            <CartTotal />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
 };
 
 export default Cart;
