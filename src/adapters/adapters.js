cd59f8024286b78ecdb5de2f71dc710da3034ec7
Yulia Karpova <karpova.jul@gmail.com>
2020-11-04 23:43:07 +1100

    Add adapter for pizza

---
+++ src/adapters/adapters.js
@@ -0,0 +1,32 @@
+const createPizza = (pizza) => {
+  return {
+    "id": pizza.id,
+    "image": pizza.image,
+    "name": pizza.name,
+    "type": pizza.type,
+    "rating": pizza.rating,
+    "kcal": pizza.kcal,
+    "ingredients": {
+      "sauces": pizza.ingredients.sauces,
+      "mainIngredients": pizza.ingredients.main_ingredients,
+      "toppings": pizza.ingredients.toppings
+    },
+    "prices": {
+      "thin": {
+        "small": pizza.prices.thin.small,
+        "medium": pizza.prices.thin.medium,
+        "large": pizza.prices.thin.large,
+      },
+      "thick": {
+        "small": pizza.prices.thick.small,
+        "medium": pizza.prices.thick.medium,
+        "large": pizza.prices.thick.large,
+      }
+    },
+    "discountPercent": pizza.discount_percent,
+    "isNew": pizza.is_new,
+    "isTop": pizza.is_top
+  }
+}
+
+export { createPizza };
