5ae673a9eb44adeff5925965b1a96c38fb64426d
Yulia Karpova <karpova.jul@gmail.com>
2020-11-06 00:52:40 +1100

    Implement TagsFilter

--- src/components/tags-filter/tags-filter.jsx
+++ src/components/tags-filter/tags-filter.jsx
@@ -1,28 +1,45 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./tags-filter.css";
 
-const TagsFilter = () => {
+export const Tag = {
+  NEW: `New pizzas`,
+  TOP: `Top rated`,
+  DISCOUNT: `Discounts`
+}
+
+const tags = Object.values(Tag);
+
+const TagsFilter = ({currentTag, onTagChange}) => {
+  const renderTagItems = tags.map(tag => {
+    const activeClassName = currentTag === tag ? `active-item` : ``;
+
+    return (
+      <li key={tag} className={`tags-list-item ${activeClassName}`}>
+        <button
+          onClick={(evt) => {onTagChange(evt.target.value)}}
+          className="button tags-list-button"
+          type="button"
+          value={tag}
+        >
+          {tag}
+        </button>
+      </li>
+    );
+  });
+
   return (
     <>
       <ul className="tags-list">
-        <li className="tags-list-item active-item">
-          <button className="button tags-list-button" type="button">
-            New Pizzas
-          </button>
-        </li>
-        <li className="tags-list-item">
-          <button className="button tags-list-button" type="button">
-            Top Rated
-          </button>
-        </li>
-        <li className="tags-list-item">
-          <button className="button tags-list-button" type="button">
-            Discounts
-          </button>
-        </li>
+        {renderTagItems}
       </ul>
     </>
   );
 };
+
+TagsFilter.propTypes = {
+  currentTag: PropTypes.string.isRequired,
+  onTagChange: PropTypes.func.isRequired
+}
 
 export default TagsFilter;
