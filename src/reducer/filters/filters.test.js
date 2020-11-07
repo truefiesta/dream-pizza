9234955660a26a6c5fe969a3a6214d40cf389c73
Yulia Karpova <karpova.jul@gmail.com>
2020-11-07 13:33:27 +1100

    Add tests for filters reducer

---
+++ src/reducer/filters/filters.test.js
@@ -0,0 +1,27 @@
+import { reducer, ActionCreator, ActionType, initialState } from "./filters.js";
+import { Tag } from "../../components/tags-filter/tags-filter.jsx";
+
+describe(`Action creators`, () => {
+  it(`changeTag returns correct type and payload`, () => {
+    const tag = Tag.NEW;
+    expect(ActionCreator.changeTag(tag)).toEqual({
+      type: ActionType.CHANGE_TAG,
+      payload: tag
+    });
+  });
+});
+
+describe(`Reducer`, () => {
+  it(`without additional parameters should return initial state`, () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  describe(`action changeTag`, () => {
+    it(`sets new tag`, () => {
+      const newTag = Tag.TOP;
+      const newState = reducer(undefined, ActionCreator.changeTag(newTag));
+      const expectedState = Object.assign({}, initialState, {currentTag: newTag});
+      expect(newState).toEqual(expectedState);
+    });
+  });
+});
