bea4c425e5f11597da60a151839d4da9eedca3c9
Yulia Karpova <karpova.jul@gmail.com>
2020-11-09 00:59:33 +1100

    Add selector to find pizza by id

--- src/reducer/pizzas/selectors.js
+++ src/reducer/pizzas/selectors.js
@@ -5,3 +5,8 @@ const NAME_SPACE = NameSpace.PIZZAS;
 export const selectPizzas = state => state[NAME_SPACE].pizzas;
 
 export const selectFavorites = state => state[NAME_SPACE].favoritePizzas;
+
+export const selectPizzaById = (pizzaId) => (state) => {
+  return selectPizzas(state)
+    .find(pizza => pizza.id === pizzaId);
+};
