e2da15af660d40c5ec08dbf5e52ca64092497c41
Yulia Karpova <karpova.jul@gmail.com>
2020-11-10 23:40:41 +1100

    Add Favorites page

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectPizzas, selectFavorites } from "../reducer/pizzas/selectors.js";
+import { getItemsForPageNumber } from "../utils.js";
+
 import Breadcrumbs from "../components/breadcrumbs/breadcrumbs.jsx";
 import CardsList from "../components/cards-list/cards-list.jsx";
 import PrevNextControls from "../components/prev-next-controls/prev-next-controls.jsx";
 
+const MAX_FAVORITES_PER_PAGE = 3;
+
 const Favorites = () => {
+  const favorites = useSelector(selectFavorites);
+  const pizzas = useSelector(selectPizzas);
+  const [page, setPage] = useState(1);
+  let favoritePizzas = [];
+  const favoritesNumber = favorites.length;
+  const noFavorites = favoritesNumber === 0;
+  if (!noFavorites) {
+    favoritePizzas = favorites.map(favoriteId => pizzas.find(pizza => pizza.id === favoriteId));
+  }
+
+  let favoritesToShow = getItemsForPageNumber(page, MAX_FAVORITES_PER_PAGE, favoritePizzas);
+
   return (
     <main className="favorites-page">
       <div className="wrapper">
@@ -12,17 +30,18 @@ const Favorites = () => {
         <div className="content">
           <section className="favorites-list-section">
             <div className="favorites-top-row">
-              <h2 className="section-title">From our menu</h2>
-              <PrevNextControls />
+              <h2 className="section-title">{noFavorites ? `You don't have any favorites yet`: `From our menu`}</h2>
+              { favoritePizzas.length > MAX_FAVORITES_PER_PAGE &&
+                <PrevNextControls
+                  currentPage={page}
+                  itemsNumber={favoritesNumber}
+                  maxItemsPerPage={MAX_FAVORITES_PER_PAGE}
+                  onPrevClick={(page) => {setPage(page)}}
+                  onNextClick={(page) => {setPage(page)}}
+                />
+              }
             </div>
-            <CardsList />
-          </section>
-          <section className="favorites-list-section">
-            <div className="favorites-top-row">
-              <h2 className="section-title">Created by me</h2>
-              <PrevNextControls />
-            </div>
-            <CardsList />
+            <CardsList pizzas={favoritesToShow} />
           </section>
         </div>
       </div>
