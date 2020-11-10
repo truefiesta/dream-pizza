e096fc15e1aec8e84282955f8ad2ebd423e7277b
Yulia Karpova <karpova.jul@gmail.com>
2020-11-10 22:42:57 +1100

    Change CardCart

--- src/components/card-cart/card-cart.jsx
+++ src/components/card-cart/card-cart.jsx
@@ -1,32 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { Operation as PizzasOperation } from "../../reducer/pizzas/pizzas.js";
+import { selectPizzaById } from "../../reducer/pizzas/selectors.js";
+import { MIN_PIZZA_QUANTITY } from "../../const.js";
+import { capitalize } from "../../utils.js";
+
 import PlusMinusButtons from "../plus-minus-buttons/plus-minus-buttons.jsx";
 
 import "./card-cart.css";
-import smallPizza from "../../assets/img/pizza-small.jpg";
 
-const CardCart = () => {
+const CardCart = ({ cartObj }) => {
+  const [quantity, setQuantity] = useState(cartObj.quantity);
+  const pizzaById = useSelector(selectPizzaById(cartObj.pizzaId));
+  const dispatch = useDispatch();
+
+  const { id, crust, size, pricePerOne } = cartObj;
+  const { image, type, name, ingredients } = pizzaById;
+
+  const allIngredients = [...ingredients.sauces, ...ingredients.mainIngredients, ...ingredients.toppings].join(', ');
+
+
+  const increment = () => {
+    setQuantity(prev => prev + 1);
+    dispatch(PizzasOperation.increaseItemQuantityInCart(id));
+  }
+
+  const decrement = () => {
+    setQuantity(prev => prev - 1);
+    dispatch(PizzasOperation.decreaseItemQuantityInCart(id));
+  }
+
   return (
     <li className="card-cart-item">
-      <button className="card-cart-remove" type="button">Remove</button>
+      <button
+        onClick={() => dispatch(PizzasOperation.removeFromCart(id))}
+        className="card-cart-remove"
+        type="button">
+          Remove
+      </button>
       <div className="card-cart-img">
-        <img src={smallPizza} width="58" height="58" alt="Photo of Pepperoni pizza" />
+        <img src={image} width="58" height="58" alt="Photo of Pepperoni pizza" />
       </div>
       <div className="card-cart-details">
         <div className="card-cart-top">
-          <p className="card-cart-type">Traditional</p>
-          <h2 className="card-cart-title">Pepperoni</h2>
+          <p className="card-cart-type">{capitalize(type)}</p>
+          <h2 className="card-cart-title">{capitalize(name)}</h2>
           <p className="card-cart-description">
-            14”, thin crust, marinara sauce,
-            pepperoni, mozzarella, fresh basil
+            {size} inches, {crust} crust, {allIngredients}
           </p>
         </div>
         <div className="card-cart-bottom">
-          <PlusMinusButtons />
-          <b className="card-cart-price">$ 26</b>
+          <PlusMinusButtons
+            currentValue={quantity}
+            minPossibleValue={MIN_PIZZA_QUANTITY}
+            onMinusClick={decrement}
+            onPlusClick={increment}
+          />
+          <b className="card-cart-price">$ {pricePerOne}</b>
         </div>
       </div>
     </li>
   );
 };
+
+CardCart.propTypes = {
+  cartObj: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    pizzaId: PropTypes.string.isRequired,
+    crust: PropTypes.string.isRequired,
+    size: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    pricePerOne: PropTypes.number.isRequired
+  }).isRequired
+}
 
 export default CardCart;
