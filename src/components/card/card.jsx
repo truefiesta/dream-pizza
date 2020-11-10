f8031b141d81722fc45f597a86ea6ae3b7947911
Yulia Karpova <karpova.jul@gmail.com>
2020-11-10 16:29:24 +1100

    Move options and prices from Card to ModalAddToCart

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,64 +1,38 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
+import { ActionCreator as PizzasAcrionCreator } from "../../reducer/pizzas/pizzas.js";
 import { Link } from "react-router-dom";
-import { Operation as PizzasOperation } from "../../reducer/pizzas/pizzas.js";
 import { capitalize, convertStarRatingToWidthPercent } from "../../utils.js";
-import { MIN_PIZZA_QUANTITY, PIZZA_OPTION, CRUST_TYPE, crustTypes, PIZZA_SIZE, pizzaSizes, InchesToSizeTitle, FavoritesClass } from "../../const.js";
+import { FavoritesClass } from "../../const.js";
 
-import CardOptionsRow from "../card-options-row/card-options-row.jsx";
 import FavoritesButton from "../favorites-button/favorites-button.jsx";
-import PlusMinusButtons from "../plus-minus-buttons/plus-minus-buttons.jsx";
-import Modal from "../modal/modal.jsx";
+import ModalContainer from "../modal-container/modal-container.jsx";
+import ModalAddToCart from "../modal-add-to-cart/modal-add-to-cart.jsx";
+
 import "./card.css";
 import newPizza from "../../assets/img/new-pizza.jpg";
 
 const Card = ({ newCard, pizza }) => {
-  const [crust, setCrust] = useState(CRUST_TYPE.THIN);
-  const [size, setSize] = useState(PIZZA_SIZE.MEDIUM);
-  const [toggleModal, setToggleModal] = useState(false);
-  const [quantity, setQuantity] = useState(MIN_PIZZA_QUANTITY);
+  const [openModal, setOpenModal] = useState(false);
   const dispatch = useDispatch();
 
   const {id, image, name, type, rating, kcal, ingredients, prices, discountPercent, isNew, isTop} = pizza;
   const allIngredients = [...ingredients.sauces, ...ingredients.mainIngredients, ...ingredients.toppings].join(', ');
 
-  let previousPrice, currentPrice, previousPriceMarkup;
+  let currentSmallestPrice, previousSmallestPrice, previousSmallestPriceMarkup;
   if (discountPercent > 0) {
-    previousPrice = prices[crust][InchesToSizeTitle[size]];
-    currentPrice = (previousPrice - (previousPrice * discountPercent / 100)).toFixed(0);
-    previousPriceMarkup = (
+    previousSmallestPrice = prices.thin.small;
+    currentSmallestPrice = (previousSmallestPrice - (previousSmallestPrice * discountPercent / 100)).toFixed(0);
+    previousSmallestPriceMarkup = (
       <span className="card-price-previous">
-        <span className="card-price-previous-number">$ {previousPrice}</span>
+        <span className="card-price-previous-number">$ {previousSmallestPrice}</span>
         <span className="card-price-previous-line"></span>
       </span>
     );
   } else {
-    currentPrice = prices[crust][InchesToSizeTitle[size]];
-    previousPriceMarkup = '';
-  }
-
-  const increment = () => {
-    setQuantity(prev => prev + 1);
-  }
-
-  const decrement = () => {
-    setQuantity(prev => prev - 1);
-  }
-
-  const createPizzaCartObject = () => {
-    return {
-      pizzaId: id,
-      crust,
-      size,
-      quantity,
-      pricePerOne: currentPrice
-    }
-  };
-
-  const addToCartHandle = () => {
-    dispatch(PizzasOperation.addToCart(createPizzaCartObject()))
-    setQuantity(MIN_PIZZA_QUANTITY);
+    currentSmallestPrice = prices.thin.small;
+    previousSmallestPriceMarkup = '';
   }
 
   const discountLabel = discountPercent > 0 ? <div className="card-mark mark-sale">{`-${discountPercent}%`}</div> : '';
@@ -131,29 +105,10 @@ const Card = ({ newCard, pizza }) => {
                 {capitalize(allIngredients)}
               </p>
             </div>
-            <div className="card-details-bottom">
-              <div className="card-options-wrapper">
-                <CardOptionsRow
-                  cardId={id}
-                  title={PIZZA_OPTION.CRUST}
-                  checkedOption={crust}
-                  options={crustTypes}
-                  onOptionChange={(crust) => {setCrust(crust)}}
-                />
-                <CardOptionsRow
-                  cardId={id}
-                  title={PIZZA_OPTION.SIZE}
-                  checkedOption={size}
-                  options={pizzaSizes}
-                  onOptionChange={(size) => {setSize(size)}}
-                />
-              </div>
-              <p className="card-options-chosen-note">{capitalize(crust)} crust, {size} inches size</p>
-            </div>
             <p className="card-price">
               <small className="card-price-note">Starts from</small>
-              <b className="card-price-current">$ {currentPrice}</b>
-              {previousPriceMarkup}
+              <b className="card-price-current">$ {currentSmallestPrice}</b>
+              {previousSmallestPriceMarkup}
             </p>
           </div>
           <FavoritesButton
@@ -161,7 +116,7 @@ const Card = ({ newCard, pizza }) => {
             className={FavoritesClass.BASIC}
           />
           <button
-            onClick={() => setToggleModal(true)}
+            onClick={() => {dispatch(PizzasAcrionCreator.setCurrentPizza(pizza)); setOpenModal(true)}}
             type="button"
             className="card-button cart-button card-not-added"
           >
@@ -191,50 +146,15 @@ const Card = ({ newCard, pizza }) => {
           </button>
         </li>
       )}
-      <Modal
-        onClose={() => {setToggleModal(false)}}
-        show={toggleModal}
-      >
-        <div className="card-modal">
-          <h2 className="modal-title">{capitalize(name)}</h2>
-          <div className="card-options-wrapper modal-card-options-wrapper">
-            <CardOptionsRow
-              cardId={id}
-              title={PIZZA_OPTION.CRUST}
-              checkedOption={crust}
-              options={crustTypes}
-              onOptionChange={(crust) => {setCrust(crust)}}
-            />
-            <CardOptionsRow
-              cardId={id}
-              title={PIZZA_OPTION.SIZE}
-              checkedOption={size}
-              options={pizzaSizes}
-              onOptionChange={(size) => {setSize(size)}}
-            />
-          </div>
-          <div className="modal-price-block">
-            <b className="card-price-current">$ {currentPrice}</b>
-            <PlusMinusButtons
-              currentValue={quantity}
-              minPossibleValue={MIN_PIZZA_QUANTITY}
-              onMinusClick={decrement}
-              onPlusClick={increment}
-              />
-          </div>
-          <div className="modal-total">
-            <p>Total:</p>
-            <p>$ {currentPrice * quantity}</p>
-          </div>
-          <button
-            onClick={addToCartHandle}
-            type="button"
-            className="modal-add-to-cart dark-button"
-          >
-            Add to cart
-          </button>
-        </div>
-      </Modal>
+      {openModal && (
+        <ModalContainer
+          setOpen={setOpenModal}
+        >
+          <ModalAddToCart
+            setClose={setOpenModal}
+          />
+        </ModalContainer>
+      )}
     </>
   );
 };
